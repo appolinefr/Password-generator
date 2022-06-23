5166e3d93658ab7912d8907658b60ac172272511
Appoline Cogan <appolinecogan@gmail.com>
2022-06-23 23:33:14 +0900

    fixed generate password function and added randomising function

--- app.js
+++ app.js
@@ -1,85 +1,172 @@
-// Write password to the #password input
+//button is clickable and needs to execute start of generate function
+
+var generateBtn = document.querySelector("#generate");
+
 function writePassword() {
-  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
-  
 }
 
-  var generateBtn = document.querySelector("#generate"); 
-  
-  generateBtn.addEventListener("click", generatePassword);
-  
-specialCharacterSelected = ["!", "”", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", ">", "?", "@", "_", "`", "^", "[", "]", "{", "}", "|", "~"];
-numericCharacterSelected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-uppercaseCharacterSelected = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-lowercaseCharacterSelected = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-generatedList = []; 
-
-var generatedPassword = (generatedList.join(''));
-
-// look into for in loop so that the numbers of characters are matching the desired length
+//// prompt with password length & validate length >8 & <128
+// if the length is outside of this scope the fonction will stop
 
 function generatePassword() {
+  const passwordLength = prompt(
+    "Please choose a password length of at least 8 characters and maximum 128 characters"
+  );
 
-  let passwordLength = prompt("Please choose a password length between between 8 and 128 characters");
-  
-  if (passwordLength < 8 ) {alert("You need at least 8 characters");
-} else if (passwordLength > 128) {alert("The maximum length is 128 characters")
-} else {generateSpecialCharacter()}; 
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("invalid number of characters, please try again");
+    return;
+  }
+
+  // array receiving confirms for all types of characters
+  selection = [];
+
+  //get special characters with confirm
+  const specialCharacter = confirm(
+    "Would you like to include special characters?"
+  );
+
+  // arrays with all possible special characters
+  var specialCharsSelection = [
+    "!",
+    "”",
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    ">",
+    "?",
+    "@",
+    "_",
+    "`",
+    "^",
+    "[",
+    "]",
+    "{",
+    "}",
+    "|",
+    "~",
+  ];
+
+  // if special characters is selected, store in selection array
+  if (specialCharacter) {
+    selection.push(specialCharsSelection);
+  }
+
+  //get numeric characters with confirm
+  const numericCharacter = confirm(
+    "Would you like to include numeric characters?"
+  );
+  // arrays with all possible numeric characters
+  var numericCharsSelection = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  // if numeric characters is selected, store in selection array
+  if (numericCharacter) {
+    selection.push(numericCharsSelection);
+  }
+
+  //get uppercase characters with confirm
+  const uppercaseCharacter = confirm(
+    "Would you like to include uppercase characters?"
+  );
+
+  // arrays with all possible uppercase characters
+  var uppercaseCharsSelection = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  // if upppercase characters is selected, store in selection array
+  if (uppercaseCharacter) {
+    selection.push(uppercaseCharsSelection);
+  }
+
+  //get lowercase characters with confirm
+  const lowercaseCharacter = confirm(
+    "Would you like to include lowercase characters?"
+  );
+
+  // arrays with all possible ulowercase characters
+  const lowercaseCharsSelection = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  // if lower characters is selected, store in selection array
+  if (lowercaseCharacter) {
+    selection.push(lowercaseCharsSelection);
+  }
+
+  var randomisedPassword = selection.join("");
+
+  for (var i = 0; i < passwordLength; i++) {
+    selection[Math.floor(Math.random() * selection.length)];
+  }
+  return randomisedPassword;
 }
-
-
-function generateSpecialCharacter (){
-  
-  specialCharacter = confirm("Would you like to include special characters?"); 
-  
-  index = Math.floor(Math.random() * specialCharacterSelected.length);
-  var specialCharacterChoice = specialCharacterSelected[index];
-  
-  if (specialCharacter) {generatedList.push(specialCharacterChoice)};
-};
-
-
-function generateNumericCharacter () {
-  
-  numericCharacter = confirm("Would you like to include numeric characters?"); 
-  
-  index = Math.floor(Math.random() * numericCharacterSelected.length);
-  numericCharacterChoice = numericCharacterSelected[index];
-  
-  if (numericCharacter) {generatedList.push(numericCharacterChoice)} 
-};
-
-
-function generateUppercaseCharacter () {
-  
-  uppercaseCharacter = confirm("Would you like to include uppercase characters?"); 
-  
-  index = Math.floor(Math.random() * uppercaseCharacterSelected.length);
-  upperCharacterChoice = uppercaseCharacterSelected[index];
-  
-  if (uppercaseCharacter) {generatedList.push(upperCharacterChoice)};
-};
-
-
-function generateLowercaseCharacter (){
-  
-  lowercaseCharacter = confirm("Would you like to include lowercase characters?"); 
-  
-  index = Math.floor(Math.random() * lowercaseCharacterSelected.length);
-  lowerCharacterChoice = lowercaseCharacterSelected[index];
-  
-  if (lowercaseCharacter) {generatedList.push(lowerCharacterChoice)} ;
-}
-
-
-
-
-
-
-
-
+generateBtn.addEventListener("click", writePassword);
